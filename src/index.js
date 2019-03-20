2de172e3eb75c2bf8c58cee0e6c982ee4634da9b
Dmitry <36167942+SifiFox@users.noreply.github.com>
2019-03-20 12:07:54 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let stack = [];
+for (let i = 0; i < str.length; i++) {
+  for (let j = 0; j < bracketsConfig.length; j++) {
+    if (str[i] == bracketsConfig[j][0]) {
+      if (bracketsConfig[j][0] == bracketsConfig[j][1] && bracketsConfig[j][1] == stack[stack.length - 1]) {
+        stack.pop();
+        continue;
+      }
+      stack.push(bracketsConfig[j][0]);
+      continue;
+    }
+    if (str[i] == bracketsConfig[j][1]) {
+      if(stack.pop() != bracketsConfig[j][0]) {
+        return false;
+      }
+    }
+  }
+}
+if (stack.length !== 0) {
+  return false;
+}
+return true;
 }
